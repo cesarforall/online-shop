8328fa44b2ec925e0f274cf41049533eba916673
Cesar Almeida <cesaralmeida89@gmail.com>
2023-10-21 16:50:31 +0200

    add key to render MyOrders properly

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -6,13 +6,12 @@ import './MyOrders.css'
 function MyOrders () {
   const context = useContext(ShoppingCardContext)
   const { orders } = context
-  console.log(orders)
   return (
     <div className='MyOrders'>
       <h1 className='font-semibold'>My Orders</h1>
       <div className='flex flex-col gap-4 pt-4'>
         {
-          orders.length === 0 ? 'There is not orders' : orders.map((order) => <OrdersCard key={order?.date + order?.totalPrice} date={order.date} totalProducts={order.totalProducts} totalPrice={order.totalPrice} />)
+          orders.length === 0 ? 'There is not orders' : orders.map((order, index) => <OrdersCard key={order?.date + order?.totalPrice} date={order.date} totalProducts={order.totalProducts} totalPrice={order.totalPrice} id={index} />)
         }
       </div>
     </div>
