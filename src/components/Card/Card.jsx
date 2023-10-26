13926f5fabf03258898c70fce13e525987e9bec3
Cesar Almeida <cesaralmeida89@gmail.com>
2023-10-26 19:37:40 +0200

    add border to card image

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -18,7 +18,7 @@ function Card ({ product, addProductToShoppingCart }) {
     >
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{category.name}</span>
-        <img className='w-full h-full object-cover rounded-lg' src={image} alt={title} />
+        <img className='w-full h-full object-cover rounded-lg border' src={image} alt={title} />
         {
           !isAddedToCart
             ? <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'><PlusIcon onClick={(e) => { e.stopPropagation(); addProductToShoppingCart(product) }} /></div>
