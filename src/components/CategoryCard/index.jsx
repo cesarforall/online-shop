7f00359387e183be902efaf9bab424e1a21997f1
Cesar Almeida <cesaralmeida89@gmail.com>
2023-12-02 19:19:16 +0100

    change color and bg CategoryCard name

--- src/components/CategoryCard/index.jsx
+++ src/components/CategoryCard/index.jsx
@@ -3,7 +3,7 @@ function CategoryCard ({ category }) {
   return (
     <div className='w-full h-[200px]'>
       <figure className='relative mb-2 w-full h-full'>
-        <span className='w-full absolute bottom-1/2 left-0 font-semibold text-2xl bg-white/60 rounded-lg text-gray-300'>{name}</span>
+        <span className='w-full absolute bottom-1/2 left-0 font-semibold text-3xl rounded-lg text-white'>{name}</span>
         <img className='w-full h-full object-cover rounded-lg border' src={image} alt={name} />
       </figure>
     </div>
