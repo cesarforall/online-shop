53915a0b3831c7d93cb29ab90721bffe0d05a1f5
Cesar Almeida <cesaralmeida89@gmail.com>
2023-12-02 09:11:40 +0100

    improve responsive CategoryCard

--- src/components/CategoryCard/index.jsx
+++ src/components/CategoryCard/index.jsx
@@ -1,8 +1,8 @@
-function Card ({ category }) {
+function CategoryCard ({ category }) {
   const { name, image } = category
   return (
-    <div className='h-[200px]'>
-      <figure className='relative mb-2 w-full h-4/5'>
+    <div className='w-full h-[200px]'>
+      <figure className='relative mb-2 w-full h-full'>
         <span className='w-full absolute bottom-1/2 left-0 font-semibold text-2xl bg-white/60 rounded-lg text-gray-300'>{name}</span>
         <img className='w-full h-full object-cover rounded-lg border' src={image} alt={name} />
       </figure>
@@ -10,4 +10,4 @@ function Card ({ category }) {
   )
 }
 
-export default Card
+export default CategoryCard
